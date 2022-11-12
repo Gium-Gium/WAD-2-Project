f3292aadd5434428f3538923e0e29bd94c7d7801
wja999 <115869409+wja999@users.noreply.github.com>
2022-11-13 03:36:08 +0800

    Add files via upload

---
+++ Guide compilation/GuidePage2CexvsDex/js/words.js
@@ -0,0 +1,11 @@
+let words = [
+    {
+        word: "Centralised",
+        hint: "Control of an activity or organization) under a single authority"
+    },
+    {
+        word: "Liquidity",
+        hint: "Measurement of how quickly its assets can be converted to cash in the short-term to meet short-term debt obligations"
+    },
+    
+]
