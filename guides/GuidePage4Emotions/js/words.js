3b3b6901368a9aa9f82a0bfc09486dca43adf298
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2022-11-13 14:11:43 +0800

    compilation of all 6 guides with game refresh
    these guides have no padding-bottom 250px at from the top paragraph id="intro". Please consolidate later ty

---
+++ guides/GuidePage4Emotions/js/words.js
@@ -0,0 +1,14 @@
+let words = [
+    {
+        word: "Backtest",
+        hint: "What you should do with your plan before executing in real markets"
+    },
+    {
+        word: "Risk-Management Plan",
+        hint: "(Two words) Plan that contains all the risk assessment, analysis, tolerance, and mitigation considerations"
+    },
+    {
+        word: "Trading Psychology",
+        hint: "(Two words) is about controlling your emotions. Knowing where the market sentiment is currently at as well as your individual emotions and sentiments."
+    },
+]
